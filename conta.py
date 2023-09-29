8bba168e72eac5ec1e07611a1b79b14de136c9f7
Arthur Lola Oliveira <arthurlolaoliveira@hotmail.com>
2023-09-28 21:35:05 -0300

    Finalizando

--- conta.py
+++ conta.py
@@ -1,34 +1,57 @@
+import datetime
+
+class Historico:
+    def __init__(self):
+        self.data_abertura = datetime.datetime.today()
+        self.transacoes = []
+    
+    def imprime(self):
+        print("Data abertura: {}".format(self.data_abertura))
+        print("Transações: ")
+        for t in self.transacoes:
+            print("-", t)
+
+
 class Cliente:
     
     def __init__(self, nome, sobrenome, cpf):
         self.nome = nome
         self.sobrenome = sobrenome
         self.cpf = cpf
+    
+class Data:
 
-
+    def __init__(self, dia, mes, ano):
+        self.dia = dia
+        self.mes = mes
+        self.ano = ano
 
 class Conta:
     
-    def __init__(self, numero, titular, saldo, limite):
+    def __init__(self, numero, cliente, saldo, limite, data_abertura):
         self.numero = numero
-        self.titular = titular
+        self.titular = cliente
         self.saldo = saldo
         self.limite = limite
-
-    
+        self.data_abertura = data_abertura
+        self.historico = Historico()
 
     def deposita(self, valor):
         self.saldo += valor
+        self.historico.transacoes.append("Depósito de {}".format(valor))
 
     def saca(self, valor):
         if(valor > self.saldo):
             return False
         else:
             self.saldo -= valor
+            self.historico.transacoes.append("Saque de {}".format(valor))
             return True
 
     def extrato(self):
-        print('Número: {} \nSaldo: {}'.format(self.numero, self.saldo))
+        print('Nome: {} \nSobrenome: {} \nCPF: {} \nNúmero: {} \nData de criação: {}/{}/{} \nSaldo: {}'.format(self.titular.nome, self.titular.sobrenome, self.titular.cpf, self.numero, self.data_abertura.dia, self.data_abertura.mes, self.data_abertura.ano, self.saldo))
+        print("----------------------------")
+        self.historico.transacoes.append("Tirou extrato - Saldo de {}".format(self.saldo))
 
     def transfere_para(self, destino, valor):
         retirou = self.saca(valor)
@@ -36,4 +59,5 @@ class Conta:
             return False
         else:
             destino.deposita(valor)
+            self.historico.transacoes.append("Transferência de {} para conta {}".format(valor, destino))
             return True
--- conta_teste.py
+++ conta_teste.py
@@ -1,9 +1,19 @@
 from conta import Conta
+from conta import Cliente
+from conta import Data
 
-conta = Conta('1234-0', 'Lola', 5000, 2000 )
-conta2 = Conta('5678-0', 'Gustavo', 7000, 4500)
+
+data_abertura = Data(9,2,2017)
+data_abertura2 = Data(12,3,2020)
+cliente = Cliente('Arthur', 'Lola', "70660067471")
+cliente2 = Cliente('Gustavo', 'Emanuel', '67668557432')
+conta = Conta('1234-0', cliente, 5000, 2000, data_abertura)
+conta2 = Conta('5678-0', cliente2, 7000, 4500, data_abertura2)
+
 
 conta.transfere_para(conta2, 1000)
 
 conta.extrato()
 conta2.extrato()
+
+conta.historico.imprime()
